eb40a29edb956c65ed924895f1b9efeb3fe63854
Mikhail Karchevskii <mikarchevski@yandex.ru>
2023-12-20 15:24:00 +0300

    Add simple pup-up & validation

--- Test landing/scripts/script.js
+++ Test landing/scripts/script.js
@@ -50,3 +50,121 @@ var swiper4 = new Swiper(".mySwiper4", {
     swiper: swiper3,
   },
 });
+let popup = document.getElementById('popup');
+let body = document.querySelector('body');
+let wrap = document.querySelector('.popup-wrap');
+let openPopupButtons = document.querySelectorAll('.form-btn');
+
+function popupToggle(){
+  
+  body.classList.toggle('no-scroll');
+  popup.classList.toggle('active');
+  wrap.classList.toggle('popup-wrap--active');
+  clearInputs();
+}
+
+document.getElementById("popup-submit").onclick = function(e){
+  let name = document.getElementById("input-name").value;
+  document.getElementById("label__text").innerText = name + " спасибо за проявленный инетрес";
+}
+
+document.addEventListener('keydown', function(event) {
+  const key = event.key; // const {key} = event; in ES6+
+      if (key === "Escape") {
+          removePopup();
+          clearInputs();
+      }
+  });
+
+function removePopup(){
+    wrap.classList.remove('popup-wrap--active');
+    popup.classList.remove('active');
+    document.getElementById('input-name').classList.remove('alert');
+    document.getElementById('input-phone').classList.remove('alert');
+    document.getElementById('input-email').classList.remove('alert');
+}
+
+function clearInputs(){
+    document.getElementById('input-name').value = '';
+    document.getElementById('input-phone').value = '';
+    document.getElementById('input-email').value = '';
+}
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+const input = document.getElementById('input-email');
+console.log(input);
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.cssText = 'border: green';
+  } else {
+    input.style.cssText = 'border: 2px solid red';
+    return false;
+  }
+}
+
+input.addEventListener('input', onInput);
+
+function isEmailValid(value) {
+return EMAIL_REGEXP.test(value);}
+
+
+function noDigits(event) {
+    if ("1234567890".indexOf(event.key) != -1)
+      event.preventDefault();
+  }
+
+
+    function isEmptyInput(){
+      var arr = [
+          document.getElementById('input-name'), 
+          document.getElementById('input-phone'),
+          document.getElementById('input-email')
+      ];
+      var count = arr.length;
+          for (let i = 0; i<arr.length;i++)
+          {
+              if (arr[i].value ===''){
+                  arr[i].classList.add('alert');
+                  count++;}
+                  else{ arr[i].classList.remove('alert');}
+
+              
+          }  
+         if (count !== arr.length){
+          return true;
+         }
+  }
+
+  openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+         if (isEmptyInput()=== true || onInput()===false)
+              { 
+                alert("Please enter"); 
+            }
+         else{
+            popupBg.classList.add('active');
+            popup.classList.add('active');
+        } 
+            
+    })
+});
+
+function rmv(input){
+  if(input.value) {
+    input.classList.remove('alert');
+  } else {
+    input.classList.add('alert');
+  }
+}
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.cssText = 'border: green';
+  } else {
+    input.style.cssText = 'border: 2px solid red';
+    return false;
+  }
+}
